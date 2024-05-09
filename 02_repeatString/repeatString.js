e33a6e369e2779557968ca07f1a9d50f5dab8ed6
xpGui <90066099+xpGui@users.noreply.github.com>
2024-05-09 16:33:19 -0300

    Add solutions for exercises 1 and 2
    That's it.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let stringHold = "";
+    for (let i = 0; i < num; i++) {
+        stringHold += string;
+    }
+    return stringHold;
 };
 
 // Do not edit below this line
